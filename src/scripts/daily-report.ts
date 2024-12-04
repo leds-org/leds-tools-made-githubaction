e5c309262a6a3b77b40f91b018f8c3171893c572
Paulo Sérgio dos Santos Júnior <paulossjunior@gmail.com>
2024-12-04 15:49:11 -0300

    criando a primeira versão do github action

---
+++ src/scripts/daily-report.ts
@@ -0,0 +1,10 @@
+import { ReportManager } from "made-report-lib";
+
+export function generateReport(): void {
+  const report = new ReportManager();
+  const dbpath = "./example";
+  report.createReport(dbpath);
+}
+
+// Executar a função
+generateReport()
